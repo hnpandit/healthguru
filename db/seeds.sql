22a1c4e1d5f0113a8605cbd7cbb4d596974752ec
pmijar <prashanth.mijar@gmail.com>
2019-03-06 18:39:01 -0500

    Adding users in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+use healthguru_db;
+insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell, height, weight) 
+values("FirstName1", "LastName1", "90013", 1970, "Male", "test1@email.com", "317-270-7000", 183, 210);
+insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell, height, weight) 
+values("FirstName2", "LastName2", "07054", 1970, "Male", "test2@email.com", "317-270-7000", 180, 190);
+use healthguru_db;
+insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell, height, weight) 
+values("FirstName3", "LastName3", "08817", 1980, "Female", "test3@email.com", "917-550-6000", 160, 140);
+use healthguru_db;
+insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell, height, weight) 
+values("FirstName4", "LastName4", "97215", 1976, "Female", "test4@email.com", "517-660-3000", 150, 120);
+use healthguru_db;
+insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell, height, weight) 
+values("FirstName5", "LastName5", "91210", 1990, "Male", "test5@email.com", "617-760-2000", 200, 220);
