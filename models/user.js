ff8488abe766a9d3b63d619b98b4d8387c97fb76
pmijar <prashanth.mijar@gmail.com>
2019-03-09 10:13:10 -0500

    updated the models files to include deleteOne function

--- models/user.js
+++ models/user.js
@@ -2,22 +2,27 @@
 var orm = require("../config/orm.js");
 
 var user = {
-  selectAll: function(cb) {
-    orm.selectAll("users", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("users", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("users", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+    selectAll: function (cb) {
+        orm.selectAll("users", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("users", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("users", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    deleteOne: function (objColVals, condition, cb) {
+        orm.deleteOne("users", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
 };
 
 module.exports = user;
--- models/userMedication.js
+++ models/userMedication.js
@@ -2,22 +2,27 @@
 var orm = require("../config/orm.js");
 
 var usermedications = {
-  selectAll: function(cb) {
-    orm.selectAll("usermedications", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("usermedications", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("usermedications", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+    selectAll: function (cb) {
+        orm.selectAll("usermedications", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("usermedications", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("usermedications", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    deleteOne: function (objColVals, condition, cb) {
+        orm.deleteOne("usermedications", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
 };
 
 module.exports = usermedications;
