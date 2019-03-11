f9d6ecd4cca3cbbcf1c2245651564542b2fb6951
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-03-11 18:07:11 -0400

    new seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -25,7 +25,7 @@ insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell,
 values("John", "Smith", "08816", 1970, "Male", "himanshu.pandit@outlook.com", "201-918-0080", 183, 210);
 
 insert into userhealthproviders (uid, hpid, lastvisit, nextvisit) 
-values(1, "1669522595", "190101", null);
+values(1, "1669522595", "190101", "190401");
 
 insert into userhealthproviders (uid, hpid, lastvisit, nextvisit) 
 values(1, "1679607451", "181222", null);
@@ -34,17 +34,17 @@ insert into usermedications (uid, medicationname, dosage, numrefill, nextrefilld
 values (1, "Norvasc", "5 mg", 2, "190326", "Hypertension");
 
 insert into usermedications (uid, medicationname, dosage, numrefill, nextrefilldate, healthcondition)
-values (1, "Metformin", "500 mg", 3, "190405", "Diabetes - Blood Sugar");
+values (1, "Metformin", "500 mg", 0, "190405", "Diabetes - Blood Sugar");
 
 insert into userprocedures (uid, procedurename, proceduredate)
-values (1, "Blood Work", "190102");
+values (1, "Blood Work", "180102");
 
 insert into userprocedures (uid, procedurename, proceduredate)
-values (1, "EKG", "180102");
+values (1, "EKG", "170102");
 
 -- Populate data for our second user Steve Ryan (Prashanth)
 insert into users (firstname, lastname, zipcode, birthyear, gender, email, cell, height, weight) 
-values("Steve", "Ryan", "08816", 1970, "Male", "rashanth.Mijar@gmail.com", "609-775-5922", 183, 210);
+values("Steve", "Ryan", "08816", 1970, "Male", "prashanth.mijar@gmail.com", "609-775-5922", 183, 210);
 
 insert into userhealthproviders (uid, hpid, lastvisit, nextvisit) 
 values(2, "1669522595", "190101", null);
