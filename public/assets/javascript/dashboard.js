250eabc2034ee33e22a6ddaf0143bb2903d50a72
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-03-13 09:39:25 -0400

    Modified seeds, formatted date and some minor changes

--- public/assets/javascript/dashboard.js
+++ public/assets/javascript/dashboard.js
@@ -1,10 +1,38 @@
+function formatDate(aDate)
+{
+  if (aDate === null)
+    return "Missing or Overdue Date";
+  else
+    return moment(aDate).format('LL'); 
+}
+
+function dateDiff(aDate)
+{
+  if (aDate === null)
+    return 0;
+  else
+  {
+    var now = moment(new Date()); //todays date
+    var end = moment(aDate); // passed in date
+    var duration = moment.duration(now.diff(end));
+    var days = duration.asDays();
+    return days;
+  }
+}
+
+
+var now = moment(new Date()); //todays date
+var end = moment("2015-12-1"); // another date
+var duration = moment.duration(now.diff(end));
+var days = duration.asDays();
 
 function displayUserDetails(id) 
 {
   $.ajax({ url: `/api/hg/${id}`, method: "GET" })
     .then(function(userData) {
-      
+  
       var counter = 1;
+      var aDate = null;
       // Let's welcome our user
       $("#username").text("Hello " + userData[0].user[0].firstname + ". Welcome to HealthGuru.");
 
@@ -17,16 +45,18 @@ function displayUserDetails(id)
         var tdCounter = $("<td>");
         tdCounter.text(counter);
 
+        //Call BettterDoctor go get
         var tdDoctor = $("<td>");
         tdDoctor.text(userData[2].provider[i].hpid);
 
         var tdLastVisit = $("<td>");
-        tdLastVisit.text(userData[2].provider[i].lastvisit);
+        tdLastVisit.text(formatDate(userData[2].provider[i].lastvisit));
 
         var tdNextVisit = $("<td>");
         if (userData[2].provider[i].nextvisit === null)
-          tdNextVisit.css("background-color", "red");
-        tdNextVisit.text(userData[2].provider[i].nextvisit);
+          tdNextVisit.css("background-color", "red")
+        
+        tdNextVisit.text(formatDate(userData[2].provider[i].nextvisit));
 
         rowRec.append(tdCounter, tdDoctor, tdLastVisit, tdNextVisit);
         $("#doctor").append(rowRec);
@@ -57,7 +87,7 @@ function displayUserDetails(id)
         tdNumRefill.text(userData[1].medication[i].numrefill);
 
         var tdNextRefillDt = $("<td>");
-        tdNextRefillDt.text(userData[1].medication[i].nextrefilldate);
+        tdNextRefillDt.text(formatDate(userData[1].medication[i].nextrefilldate));
 
         rowRec.append(tdCounter, tdCondition, tdMedName, tdDosage, tdNumRefill, tdNextRefillDt);
         $("#medication").append(rowRec);
@@ -77,7 +107,10 @@ function displayUserDetails(id)
         tdProcName.text(userData[3].procedure[i].procedurename);
 
         var tdProcDate = $("<td>");
-        tdProcDate.text(userData[3].procedure[i].proceduredate);
+
+        if (dateDiff(userData[3].procedure[i].proceduredate) > 365)
+            tdProcDate.css("background-color", "red")
+        tdProcDate.text(formatDate(userData[3].procedure[i].proceduredate));
 
         rowRec.append(tdCounter, tdProcName, tdProcDate);
         $("#procedure").append(rowRec);
