bb77a4d5498cfe192d3eafe5d670687473193886
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-03-16 13:00:41 -0400

    for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql2");
 
+// mysql://baygazrvuqacgzvf:cme6ma2sesnbquux@zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c4tnx2q1ydskjzho
+
+var connection = mysql.createConnection("mysql://baygazrvuqacgzvf:cme6ma2sesnbquux@zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c4tnx2q1ydskjzho");
+/*
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,6 +12,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "healthguru_db"
 });
+*/
 
 // Make connection.
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,54 @@
 -- Make sure we are using healthguru database
-use healthguru_db;
+use c4tnx2q1ydskjzho;
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	firstname varchar(255) NOT NULL,
+    lastname varchar(255) NOT NULL,
+    zipcode varchar(10) NOT NULL,
+    birthyear int(5) NOT NULL,
+    gender varchar(10) NOT NULL,
+    email varchar(100) NOT NULL,
+    cell  varchar(50) NOT NULL,
+    height int(4) NOT NULL,
+    weight int(4) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE userhealthproviders
+(
+	id int NOT NULL AUTO_INCREMENT,
+    uid int NOT NULL,
+    hpid varchar(50) NOT NULL,
+	lastvisit DATE NOT NULL,
+    nextvisit DATE,
+	PRIMARY KEY (id),
+    CONSTRAINT FK_userhp FOREIGN KEY (uid) REFERENCES users(id)
+);
+
+CREATE TABLE usermedications
+(
+	id int NOT NULL AUTO_INCREMENT,
+    uid int NOT NULL,
+    medicationname varchar(50) NOT NULL,
+    dosage varchar(50) NOT NULL,
+    numrefill int(4) NOT NULL,
+	nextrefilldate DATE NOT NULL,
+    healthcondition varchar(100) NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT FK_usermedication FOREIGN KEY (uid) REFERENCES users(id)
+);
+
+CREATE TABLE userprocedures
+(
+	id int NOT NULL AUTO_INCREMENT,
+    uid int NOT NULL,
+    procedurename varchar(1000) NOT NULL,
+    proceduredate DATE NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT FK_userprocedure FOREIGN KEY (uid) REFERENCES users(id)
+);
 
 -- Truncate all tables before populating
 SET FOREIGN_KEY_CHECKS=0;
